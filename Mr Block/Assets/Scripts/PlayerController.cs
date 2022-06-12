80eea77f9a8a10dcf95402c80d39f4e5f535f2c0
SreeragKR <sreeragzreeezzz@gmail.com>
2022-06-12 22:39:40 +0530

    Added UI

--- Mr Block/Assets/Scripts/PlayerController.cs
+++ Mr Block/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rb;
     public float speed = 5f;
 
+    public GameObject gameWonPanel;
+    private bool isGameWon = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,33 +19,39 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetAxis("Horizontal") > 0)
+        if(!isGameWon)
         {
-            rb.velocity = new Vector2(speed, 0f);
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                rb.velocity = new Vector2(speed, 0f);
+            }
+            else if (Input.GetAxis("Horizontal") < 0)
+            {
+                rb.velocity = new Vector2(-speed, 0f);
+            }
+            else if (Input.GetAxis("Vertical") > 0)
+            {
+                rb.velocity = new Vector2(0f, speed);
+            }
+            else if (Input.GetAxis("Vertical") < 0)
+            {
+                rb.velocity = new Vector2(0f, -speed);
+            }
+            else if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+            {
+                rb.velocity = new Vector2(0f, 0f);
+            }
         }
-        else if(Input.GetAxis("Horizontal") < 0)
-        {
-            rb.velocity = new Vector2(-speed, 0f);
-        }
-        else if(Input.GetAxis("Vertical") > 0)
-        {
-            rb.velocity = new Vector2(0f, speed);
-        }
-        else if(Input.GetAxis("Vertical") < 0)
-        {
-            rb.velocity = new Vector2(0f, -speed);
-        }
-        else if(Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
-        {
-            rb.velocity = new Vector2(0f, 0f);
-        }
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag=="WinDoor")
         {
+            gameWonPanel.SetActive(true);
             Debug.Log("Level Complete !!!");
+            isGameWon = true;
         }
     }
 }
